9bb3947a6c89167e05e7e007fe6421c0662ac92d
P VINEETH KUMAR <2018073@tcs.com>
2024-06-16 13:37:04 +0300

    Mysql

--- MySql.sh
+++ MySql.sh
@@ -36,8 +36,19 @@ VALIDATE $? "Enabling Mysql Server"
 systemctl start mysqld &>>LOGFILE
 VALIDATE $? "Starting Mysql Server"
 
-mysql_secure_installation --set-root-pass ExpenseApp@1
-VALIDATE $? "Setting up root password"
+#mysql_secure_installation --set-root-pass ExpenseApp@1
+#VALIDATE $? "Setting up root password"
+
+#Below code will be useful for idempotent nature
+mysql -h db.daws78s.online -uroot -pExpenseApp@1 -e 'show databases:' &>>LOGFILE
+if [ $? -ne 0 ]
+then
+    mysql_secure_installation --set-root-pass ExpenseApp@1 &>>LOGFILE
+    VALIDATE $? "MYSql Root Password setting up"
+else
+    echo -e "MySql root password is already setup...$Y SKIPPING $N"
+fi
+
 
 
 
